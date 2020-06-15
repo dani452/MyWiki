3d4b93a55947dfe4eb834e39d81205cfdd90e92e
Daniel Da Silva <dasilva505@gmail.com>
2020-06-15 04:20:39 +0200

    homepage_content_image_working

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,25 +27,44 @@ class HomeController extends AbstractController
 
         $search = $request->request->get('search');
         if($search){
-            $api_url = "https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles=".ucwords($search)."&redirects=true";
-            $api_url = str_replace(' ','%20',$api_url);
+            $api = "https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles=".ucwords($search)."&redirects=true";
+            $api = str_replace(' ','%20',$api);
 
-            if($data = json_decode(@file_get_contents($api_url))){
+            if($data = json_decode(@file_get_contents($api))){
                 foreach($data->query->pages as $key=>$val){
                     $pageId = $key;
                 break;
                 }
                 $content = $data->query->pages->$pageId->extract;
 
-                return $this->render('article.html.twig', [
-                    'content' => $content,
-                ]);
             } else {
-                echo 'No results found';
+                echo 'No contents found';
             }
+        
+            
+            $api_url = "https://fr.wikipedia.org/w/api.php?action=query&formatversion=2&format=json&prop=pageimages|pageterms&piprop=original&piprop=thumbnail&pithumbsize=400&pilicense=any&titles=".ucwords($search);
+            $api_url = str_replace(' ','%20',$api_url);
+
+            if($data = json_decode(@file_get_contents($api_url))){
+
+                foreach ($data->query->pages as $pages){
+
+                    $image = $pages->thumbnail->source;
+                    $title = $pages->title;
+                 }
+            } else {
+                echo 'No images found';
+            }
+
+            return $this->render('article.html.twig', [
+                'content' => $content,
+                'image'   => $image,
+                'title'   => $title
+            ]);
+        } else {
+            echo 'No results found';
         }
-
-        return $this->render('home.html.twig');
+            return $this->render('home.html.twig');
     }
-
 }
+      
