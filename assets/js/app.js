d9607f2a753535fbe307361f35446da79018b8f3
Daniel Da Silva <dasilva505@gmail.com>
2020-06-21 03:31:00 +0200

    not_display_modification_button

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,5 +10,6 @@ import '../css/app.css';
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
 
+$('.mw-editsection').css("display", "none");
